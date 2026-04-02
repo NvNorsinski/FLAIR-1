2152f9fb22abfcdbce705bf8bf54793117ca3ef8
Nils von Norsinski <Nils.vonNorsinski@statistik.gv.at>
2026-04-02 11:24:13 +0000

    added S3 bucket acces

--- src/flair/inference.py
+++ src/flair/inference.py
@@ -1,21 +1,70 @@
 # TODO
-# yaml file updates, input , output paths, S3 Bucket benutzen
+# test it
 import os
 import glob
 from pathlib import Path
 
-# Pfad zum Hauptverzeichnis
-dir_input = "/mnt/eo/projekt/2023_Essnet/results/Raster_tiled/"
-outputpath = '/home/nnors/Vektor/'
-yamlfile_output_path = '/mnt/eo/projekt/2023_Essnet/results/yamlfiles/'
+import boto3
 
+#-----------------------------------------------------------------------
+with open('/home/eouser/ec2credentials') as f:
+    content = f.read()
+list_content = content.split(sep = ':')
+
+access_key = list_content[0]
+secret_key = list_content[1]
+
+
+endpoint_url = "s3.waw3-2.cloudferro.com"
+endpoint_url_https = "https://s3.waw3-2.cloudferro.com"
+region_name = "US"
+
+try:
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        endpoint_url=endpoint_url_https,
+        region_name=region_name,
+    )
+
+    print(s3.list_buckets()["Buckets"])
+
+except Exception as issue:
+    print("The following error occurred:")
+    print(issue)
+
+#---------------------------------------------------------------------
+OUT_BUCKET = "AT_results"
+IN_BUCKET = "AT_Orthofotos"
+IN_PREFIX_RASTER = "orthofotos"
+OUT_PREFIX_RASTER = "AT/AT/2024/output/"
+
+yamlfile_output_path = '/home/eouser/yamlfiles_inference'
+
+
+def list_s3_files_in_folder_using_client(bucket_name, prefix):
+    """Return files ending with ending with '.tif' in S3 Bucket."""
+    paginator = s3.get_paginator("list_objects_v2")
+    files = []
+    for page in paginator.paginate(Bucket=bucket_name, Prefix=prefix):
+        for obj in page.get("Contents", []):
+            if obj["Key"].lower().endswith(".tif"):
+                files.append(obj["Key"])
+    return files
+
+def list_tif_files(directory):
+    """Return files ending with ending with '.tif' in Directory."""
+    return glob.glob(os.path.join(directory, "**", "*.tif"), recursive=True)
+
+# === Function to update YAML configuration files ===
 def update_yaml(input_filepath):
     def update_yaml_text(key, new_value, file_yaml_input, file_yaml_output):
         # Ensure string values are quoted
         if isinstance(new_value, str) and not (new_value.startswith('"') or new_value.startswith("'")):
             new_value = f'"{new_value}"'
 
-        #Read template file and replace values
+        # Read template file and replace values
         lines_out = []
         with open(file_yaml_input, "r", encoding="utf-8") as f:
             for line in f:
@@ -29,35 +78,28 @@ def update_yaml(input_filepath):
         with open(file_yaml_output, "w", encoding="utf-8") as f:
             f.writelines(lines_out)
 
-    # output yamlfile
+    # Paths for yaml and input/output tif
+    yamlfile = '/home/eouser/FLAIR-1/configs/flair-1-config-detect.yaml'
+
     filename = input_filepath.split('/')
     filename = filename[-1]
 
-    yamlfile = f'{yamlfile_output_path}{filename}.yaml'
+    yamlfile = input_filepath + ".yaml"
 
+    inputfile = input_filepath + '.tif'
 
+    outputname = filename
+
+    print("yaml out: " + yamlfile)
+    update_yaml_text(key="output_name", new_value=outputname, 
+                     file_yaml_input="proto.yaml", file_yaml_output=yamlfile)
     
-    inputfile_raster = f'{dir_input}{filename}.tif'
-
-
-
-    p = Path(__file__).with_name('config-vector_proto.yaml')
-    #file_yaml_input = p.absolute()
-    file_yaml_input = '/home/eouser/FLAIR-1/configs/flair-1-config-detect.yaml'
-
-    # TODO
-    update_yaml_text(key = "input_file", new_value = f'{input_filepath}.tif', 
-                     file_yaml_input = file_yaml_input, file_yaml_output = yamlfile)
-    
-    update_yaml_text(key = "output_file", new_value=outputname_geopackage, file_yaml_input=yamlfile, 
+    update_yaml_text(key="input_img_path", new_value=inputfile, file_yaml_input=yamlfile, 
                      file_yaml_output=yamlfile)
 
 
-def list_tif_files(directory):
-    return glob.glob(os.path.join(dir_input, "**", "*.tif"), recursive=True)
 
-
-list_input = list_tif_files(dir_input)
+list_input = list_s3_files_in_folder_using_client(bucket_name=IN_BUCKET, prefix=IN_PREFIX_RASTER)
 
 
 for input in list_input:
@@ -66,11 +108,8 @@ for input in list_input:
     yamlfile_name = input_filepath.split('/')
     yamlfile_name = yamlfile_name[-1]
 
-
-   
     print(yamlfile_name)
     print(input_filepath)
-
 
     update_yaml(input_filepath)
 
@@ -78,7 +117,7 @@ for input in list_input:
     yamlfile_path = f'{yamlfile_output_path}{yamlfile_name}.yaml'
     
     # # # Leerzeichen ist wichtig
-    osCommand="/home/eouser/FLAIR-1/src/flair/flair_detect.sh " + yamlfile_path
+    osCommand = "/home/eouser/FLAIR-1/src/flair/flair_detect.sh " + yamlfile_path
     print(osCommand)
 
     if os.system(osCommand)!=0:
